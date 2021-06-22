9a61036cd2b71047fe4bc91e3b8b5c0d398970c4
loverboyXD <79841865+loverboyXD@users.noreply.github.com>
2021-06-22 08:54:21 +0530

    Add files via upload

---
+++ bot/__main__.py
@@ -0,0 +1,83 @@
+import glob
+from bot import bot
+from sys import argv
+from telethon import TelegramClient
+from bot.Config import Var 
+from telebot.utils import load_module, start_mybot, load_pmbot
+from pathlib import Path
+import telethon.utils
+from bot import CMD_HNDLR
+
+GROUP = Var.PRIVATE_GROUP_ID
+BOTNAME = Var.BOT_USERNAME
+LOAD_MYBOT = Var.LOAD_MYBOT
+
+
+async def add_bot(bot_token):
+    await bot.start(bot_token)
+    bot.me = await bot.get_me()
+    bot.uid = telethon.utils.get_peer_id(bot.me)
+
+
+async def startup_log_all_done():
+    try:
+        await bot.send_message(GROUP, f"**YOU ScorpionSecurity BOT IS ON.!✨*SINCE NOw NO-ONE can spam your INBOX.SHARE\n\n    ~ @PsychoBots_chat ←SUPPORT**")
+    except BaseException:
+        print("Either PRIVATE_GROUP_ID is wrong or you have left the group.")
+
+if len(argv) not in (1, 3, 4):
+    bot.disconnect()
+else:
+    bot.tgbot = None
+    if Var.BOT_USERNAME is not None:
+        print("Initiating Inline Bot")
+        # ForTheGreatrerGood of beautification
+        bot.tgbot = TelegramClient(
+            "BOT_TOKEN",
+            api_id=Var.APP_ID,
+            api_hash=Var.API_HASH
+        ).start(bot_token=Var.BOT_TOKEN)
+        print("Initialisation finished, no errors")
+        print("Starting SCORPION")
+        bot.loop.run_until_complete(add_bot(Var.TG_BOT_USER_NAME_BF_HER))
+        print("Startup Completed")
+    else:
+        bot.start()
+
+path = 'bot/security/*.py'
+files = glob.glob(path)
+for name in files:
+    with open(name) as f:
+        path1 = Path(f.name)
+        shortname = path1.stem
+        load_module(shortname.replace(".py", ""))
+
+print("SCORPION has been deployed! ")
+
+print("Setting up TGBot")
+path = "bot/assistant/*.py"
+files = glob.glob(path)
+for name in files:
+    with open(name) as f:
+        path1 = Path(f.name)
+        shortname = path1.stem
+        start_mybot(shortname.replace(".py", ""))
+
+if LOAD_MYBOT == "True":
+    path = "bot/assistant/pmbot/*.py"
+    files = glob.glob(path)
+    for name in files:
+        with open(name) as f:
+            path1 = Path(f.name)
+            shortname = path1.stem
+            load_pmbot(shortname.replace(".py", ""))
+    print("TGBot set up completely!")
+
+print("TGBot set up - Level - Basic")
+print("Scorpion_security has been fully deployed! Do Visit @PsychoBots_chat")
+bot.loop.run_until_complete(startup_log_all_done())
+
+if len(argv) not in (1, 3, 4):
+    bot.disconnect()
+else:
+    bot.run_until_disconnected()
