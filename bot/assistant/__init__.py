80699b4e5ed1d0efadb673293fa1394116e2a8e0
loverboyXD <79841865+loverboyXD@users.noreply.github.com>
2021-06-22 15:59:08 +0530

    Add files via upload

---
+++ bot/assistant/__init__.py
@@ -0,0 +1,26 @@
+from bot import bot
+
+NAME = bot.me.first_name
+OWNER_ID = bot.me.id
+
+# START BY OTHERS
+startother = """
+Personal bot of {} to contact him👀 through me🙂
+
+This bot is maded by[Psycho Bots](https://t.me/Psycho_bots) Join for more info
+
+click BELOW TO KNOW MORE👀:
+""".format(NAME)
+
+# START BY OWNER
+startowner = """
+H! Master👀 {} How can i help you today
+join [Psycho bots](https://t.me/Psycho_bots) for more info
+let me help you by clicking options below:
+""".format(NAME)
+
+# By @Alone_loverboy
+# inspired from userbots..
+# menu to run bot....
+# Part of Lovely bots
+# important file
